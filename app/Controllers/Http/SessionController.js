120cf82aaa214507e2d4dda26ea3fc31eaca694a
diego moreno <diegosvm@hotmail.com>
2019-02-20 23:15:33 -0300

    dev.

---
+++ app/Controllers/Http/SessionController.js
@@ -0,0 +1,17 @@
+'use strict'
+
+class SessionController {
+
+  async store ({ request, response, auth }) {
+    try {
+    const { email, password } = request.all()
+    const token = await auth.attempt(email, password)
+
+    return token
+  }catch(error) {
+    return response.status(error.status).send({ error: { message: 'Algo não deu certo, a senha esta correta ?' } })
+  }
+  }
+}
+
+module.exports = SessionController
--- start/routes.js
+++ start/routes.js
@@ -4,6 +4,9 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 Route.post('users','UserController.store')
+Route.post('sessions', 'SessionController.store')
+Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
